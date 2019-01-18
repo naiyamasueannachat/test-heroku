bb1634014596fc962a7f44d7fa2f88ad20eceff2
ice <niyana_ice@hotmail.com>
2019-01-18 14:04:57 +0700

    [FIX] package json

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+// const express = require('express');
+// const app = express();
+// app.use(express.static(__dirname + '/dist'));
+// app.listem(process.env.PORT || 8080);
+
+
 //Install express server
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
-
+ 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// Replace the '/dist/my-profile'
+app.use(express.static(__dirname + '/dist/test-heroku'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/my-profile/index.html'
+  res.sendFile(path.join(__dirname + '/dist/test-heroku/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
